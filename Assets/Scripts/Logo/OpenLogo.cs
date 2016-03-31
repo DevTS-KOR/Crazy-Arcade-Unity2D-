6894ab2cfdbe612a8d8e29b1b05948225bb19ff0
TaekSoo <gjcka1234@naver.com>
2016-04-01 02:27:21 +0900

    160401

---
+++ Assets/Scripts/Logo/OpenLogo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenLogo : MonoBehaviour {
+
+    private float StartTime;
+    private float EndTime;
+    // Use this for initialization
+    void Start ()
+    {
+        StartTime = Time.deltaTime;
+    }
+
+    void Update()
+    {
+        EndTime += Time.deltaTime;
+
+        print(EndTime - StartTime);
+        if(EndTime - StartTime > 3f)
+        {
+            Application.LoadLevel("Opening");
+        }
+    }
+
+}
