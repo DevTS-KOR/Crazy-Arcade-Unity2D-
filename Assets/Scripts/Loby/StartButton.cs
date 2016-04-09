c8fe1de66b43499c383219d42e0403fcfa3fba80
TaekSoo <gjcka1234@naver.com>
2016-04-09 21:44:00 +0900

    160409

--- Assets/Scripts/Loby/StartButton.cs
+++ Assets/Scripts/Loby/StartButton.cs
@@ -17,18 +17,20 @@ public class StartButton : MonoBehaviour {
 	public void GameStartFunc()
 	{
 		//플레이어의 캐릭터 선택이 완료되어야 스타트 버튼이 작동하게 한다.
-		if (Global.FirstPlayer == true) 
+		if (Global.FirstPlayer == true && Global.SecondPlayer == true) 
 		{
 			ClickSource.PlayOneShot (ClickSound, 1f);
 
 			//코루틴으로 2초뒤에 씬이동을 실행한다.
-			StartCoroutine (delayTime (2.0f));
+			StartCoroutine (delayTime (2.0f));      //멤버변수는 초단위 시간
 		}
 	}
 
 	IEnumerator delayTime(float fSec)
 	{
 		yield return new WaitForSeconds (fSec);
+        
+        //2초가 지나면 다시 실행되며 ForestMap 씬으로 이동한다.
 		Application.LoadLevel ("ForestMap");
 	}
 
